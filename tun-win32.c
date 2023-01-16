01a5041ff5ef88fb912f83ba4ac73b680c0e3bdd
zhangqi <ZhangQ@gdyunjie.com>
2023-01-16 13:58:46 +0800

    1

--- tun-win32.c
+++ tun-win32.c
@@ -150,7 +150,9 @@ static intptr_t search_taps(struct openconnect_info *vpninfo, tap_callback *cb, 
         printf("tun-win32 step13\n");
 		found++;
 
+		printf("vpn ifName:%s,   namebuf:%s", vpninfo->ifname, namebuf->data);
 		if (vpninfo->ifname && strcmp(namebuf->data, vpninfo->ifname)) {
+			printf("tun-win32 step13.1\n");
 			vpn_progress(vpninfo, PRG_DEBUG,
 				     _("Ignoring non-matching TAP interface \"%s\"\n"),
 				     namebuf->data);
